f2fe0f9402e30b19851f6b79c30fd0cff04b2f84
Wojtek Pyda <w.pyda@poczta.onet.pl>
2018-01-11 17:09:26 +0100

    app skeleton

--- src/App.js
+++ src/App.js
@@ -1,21 +1,22 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {Component} from 'react';
+import {BrowserRouter, Route} from 'react-router-dom'
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
+    render() {
+        return (
+            <BrowserRouter>
+                <div>
+                    <AppBar/>
+                    <SideBar/>
+
+                    <Route path="/" exact={true} component={Dashboard} />
+                    <Route path="/items" component={FoodList} />
+                    <Route path="/favourties" component={Favourities} />
+                    <Route path="/addnewitem" component={AddNewItem} />
+                </div>
+            </BrowserRouter>
+        );
+    }
 }
 
 export default App;
---
+++ src/Favourities.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Favourities = () => (
+    <div>
+        <h2>Favourities</h2>
+    </div>
+)
+
+export default Favourities
