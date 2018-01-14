ec0c3ea9cddd2c8ca008fcba113659840643ae2d
Tomasz Banach <tomaszbanach01@gmail.com>
2018-01-14 12:02:49 +0100

    added product database and exported to FoodDetails

--- src/FoodDetails.js
+++ src/FoodDetails.js
@@ -1,9 +1,21 @@
 import React from 'react';
+import foodBase from './database'
 
 const FoodDetails = () => (
     <div>
-        <h2>Details of one foodie</h2>
+        <h2>Details of database name </h2>
+        <p>
+            uid: <br/>
+            name: <br/>
+            category: <br/>
+            energy: <br/>
+            protein: <br/>
+            fat: <br/>
+            carbohydrate: <br/>
+            sugars: <br/>
+            photo:
+        </p>
     </div>
 )
-
+console.log(foodBase)
 export default FoodDetails
--- src/FoodList.js
+++ src/FoodList.js
@@ -3,6 +3,9 @@ import React from 'react';
 const FoodList = () => (
     <div>
         <h2>FoodList</h2>
+        <input id='value' type='text' placeholder='choose food item'/>
+        <button id='check'>CHECK</button>
+
     </div>
 )
 
