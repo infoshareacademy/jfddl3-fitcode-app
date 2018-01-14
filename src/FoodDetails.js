98a985a13544acb749bffc353912c71fe93a1627
Tomasz Banach <tomaszbanach01@gmail.com>
2018-01-14 18:59:15 +0100

    product data displayed

--- src/FoodDetails.js
+++ src/FoodDetails.js
@@ -1,21 +1,32 @@
 import React from 'react';
 import foodBase from './database'
 
+for (var i = 0; i < foodBase.length; i++) {
+    if (foodBase[i].name === 'brokuł') {
+        var uidOfProduct = foodBase[i].uid
+        var categoryOfProduct = foodBase[i].category
+        var energyOfProduct = foodBase[i].energy
+        var proteinOfProduct = foodBase[i].protein
+        var fatOfProduct = foodBase[i].fat
+        var carbohydrateOfProduct = foodBase[i].carbohydrate
+        var sugarsOfProduct = foodBase[i].sugars
+        var photoOfProduct = foodBase[i].photo
+    }
+}
+
 const FoodDetails = () => (
     <div>
-        <h2>Details of database name </h2>
+        <h2>Details of PRODUCT NAME</h2>
         <p>
-            uid: <br/>
-            name: <br/>
-            category: <br/>
-            energy: <br/>
-            protein: <br/>
-            fat: <br/>
-            carbohydrate: <br/>
-            sugars: <br/>
-            photo:
+            uid: {uidOfProduct}<br/>
+            category: {categoryOfProduct}<br/>
+            energy: {energyOfProduct}<br/>
+            protein: {proteinOfProduct}<br/>
+            fat: {fatOfProduct}<br/>
+            carbohydrate: {carbohydrateOfProduct}<br/>
+            sugars: {sugarsOfProduct}<br/>
+            photo: {photoOfProduct}
         </p>
     </div>
 )
-console.log(foodBase)
 export default FoodDetails
