115e5ee3ab190b468895869ff2d335d9ba446db1
darkroom51 <darkroom@wp.pl>
2018-01-15 16:57:00 +0100

    json file added; FoodList first steps;

--- src/FoodList.js
+++ src/FoodList.js
@@ -1,9 +1,45 @@
-import React from 'react';
+import React, {Component} from 'react';
+import Avatar from 'material-ui/Avatar';
+import {List, ListItem} from 'material-ui/List';
+import Subheader from 'material-ui/Subheader';
+import ActionFavorite from 'material-ui/svg-icons/action/favorite';
 
-const FoodList = () => (
-    <div>
-        <h2>FoodList</h2>
-    </div>
-)
+class FoodList extends React.Component {
+    state = {
+        data: null
+    }
+
+
+    componentWillMount() {
+        fetch(
+            `${process.env.PUBLIC_URL}/database.json`
+        )
+            .then(response => response.json())
+            .then(parsedJSONData => {
+                this.setState({
+                    data: parsedJSONData
+                })
+            })
+    }
+
+    render() {
+        return (
+            <div><List> <Subheader>Test Food List</Subheader>
+                {
+                    this.state.data && this.state.data
+                        .map(
+                            product => (
+                                <ListItem
+                                    primaryText={product.name}
+                                    secondaryText={product.energy}
+                                    leftAvatar={<Avatar src={product.photo}/>}
+                                    rightIcon={<ActionFavorite/>}
+                                />
+                            ))
+                }
+            </List></div>
+        )
+    }
+}
 
 export default FoodList
