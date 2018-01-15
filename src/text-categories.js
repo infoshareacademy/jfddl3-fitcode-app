1c9d055fd3502684dad8c257f39a8b647879c3fc
Wojtek Pyda <w.pyda@poczta.onet.pl>
2018-01-15 16:56:06 +0100

    form positions added

---
+++ src/text-categories.js
@@ -0,0 +1,40 @@
+import FoodAdd from './FoodAdd'
+
+const textCategories = [
+    {
+        hintText: 'Jedzenie',
+        floatingLabelText: 'Nazwa'
+    },
+
+    {
+        hintText: 'Owoce, warzywa, itp',
+        floatingLabelText: 'Kategoria'
+    },
+
+    {
+        hintText: 'Kcal',
+        floatingLabelText: 'Kalorie'
+    },
+
+    {
+        hintText: 'Białko',
+        floatingLabelText: 'g'
+    },
+
+    {
+        hintText: 'Tłuszcze',
+        floatingLabelText: 'g'
+    },
+
+    {
+        hintText: 'Węglowodany',
+        floatingLabelText: 'g'
+    },
+
+    {
+        hintText: 'Cukry',
+        floatingLabelText: 'g'
+    },
+]
+
+export default textCategories
