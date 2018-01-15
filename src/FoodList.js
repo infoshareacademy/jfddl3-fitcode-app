068b91ad51e89d7b2fc52a53924f29a855594f04
Waldek Fryc <waldemar.fryc@gmail.com>
2018-01-15 22:38:06 +0100

    search filter fields added

--- src/FoodList.js
+++ src/FoodList.js
@@ -5,9 +5,19 @@ import {List, ListItem} from 'material-ui/List';
 import Subheader from 'material-ui/Subheader';
 import ActionFavorite from 'material-ui/svg-icons/action/favorite';
 
+import TextField from 'material-ui/TextField';
+import Slider from 'material-ui/Slider';
+import SelectField from 'material-ui/SelectField';
+import MenuItem from 'material-ui/MenuItem';
+
+
+
 class FoodList extends Component {
     state = {
-        data: null
+        data: null,
+        foodName: '',
+        kcalSlider: 150,
+        catSelect: '',
     }
 
 
@@ -23,9 +33,55 @@ class FoodList extends Component {
             })
     }
 
+
+    handleFoodName = (event, value) => {
+        this.setState({foodName: value});
+    };
+
+    handleKcalSlider = (event, value) => {
+        this.setState({kcalSlider: value});
+    };
+
+    handleCatSelect = (event, index, value) => this.setState({catSelect: value})
+
     render() {
         return (
-            <div><List> <Subheader>Test Food List</Subheader>
+            <div>
+
+                <div>
+                    <TextField
+                        // hintText="Hint Text"
+                        floatingLabelText="Search your foodie..."
+                        onChange={this.handleFoodName}
+                    />
+                    <p>
+                        <span>{this.state.foodName} , you say</span>
+                    </p>
+                    <Slider
+                        min={0}
+                        max={300}
+                        step={30}
+                        value={this.state.kcalSlider}
+                        onChange={this.handleKcalSlider}
+                    />
+                    <p>
+                        <span>{this.state.kcalSlider} Kcal</span>
+                    </p>
+                    <SelectField
+                        floatingLabelText="Categories"
+                        value={this.state.catSelect}
+                        onChange={this.handleCatSelect}
+                    >
+                        <MenuItem value={1} primaryText="warzywa" />
+                        <MenuItem value={2} primaryText="owoce" />
+                        <MenuItem value={3} primaryText="mieso" />
+                        <MenuItem value={4} primaryText="ryby" />
+                        <MenuItem value={5} primaryText="nabial" />
+                    </SelectField>
+                </div>
+
+
+                <List><Subheader>Test Food List</Subheader>
                 {
                     this.state.data && this.state.data
                         .map(
