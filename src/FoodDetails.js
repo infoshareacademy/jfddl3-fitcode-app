145ec29028dfd0cf6738d88adcad85fdec67d6ff
Tomasz Banach <tomaszbanach01@gmail.com>
2018-01-15 16:09:18 +0100

    rebuild product display

--- src/FoodDetails.js
+++ src/FoodDetails.js
@@ -1,32 +1,49 @@
 import React from 'react';
 import foodBase from './database'
 
-for (var i = 0; i < foodBase.length; i++) {
-    if (foodBase[i].name === 'brokuł') {
-        var uidOfProduct = foodBase[i].uid
-        var categoryOfProduct = foodBase[i].category
-        var energyOfProduct = foodBase[i].energy
-        var proteinOfProduct = foodBase[i].protein
-        var fatOfProduct = foodBase[i].fat
-        var carbohydrateOfProduct = foodBase[i].carbohydrate
-        var sugarsOfProduct = foodBase[i].sugars
-        var photoOfProduct = foodBase[i].photo
+const id = '7de10565-8826-40b8-9da0-adbf044b49af' //tutaj beda przekazane dane poprzez URL
+
+class FoodDetails extends React.Component {
+    state = {
+        data: null
+    }
+
+
+    componentWillMount() {
+        fetch(
+            `${process.env.PUBLIC_URL}/database.json`
+        )
+            .then(response => response.json())
+            .then(parsedJSONData => {
+                this.setState({
+                    data: parsedJSONData
+                })
+            })
+    }
+
+    render() {
+        return (
+            <div>
+                {
+                    this.state.data && this.state.data
+                        .filter(product => id === product.uid)
+                        .map(
+                            product =>
+                                <div>
+                                    <p>Nazwa : {product.name}</p>
+                                    <p>Kategoria: {product.category}</p>
+                                    <p>Kalorie: {product.energy}</p>
+                                    <p>Proteiny: {product.protein}</p>
+                                    <p>Tluszcz: {product.fat}</p>
+                                    <p>Weglowodany: {product.carbohydrate}</p>
+                                    <p>Cukry: {product.sugars}</p>
+                                    <p>Foto: {product.photo}</p>
+                                </div>
+                        )
+                }
+            </div>
+        )
     }
 }
 
-const FoodDetails = () => (
-    <div>
-        <h2>Details of PRODUCT NAME</h2>
-        <p>
-            uid: {uidOfProduct}<br/>
-            category: {categoryOfProduct}<br/>
-            energy: {energyOfProduct}<br/>
-            protein: {proteinOfProduct}<br/>
-            fat: {fatOfProduct}<br/>
-            carbohydrate: {carbohydrateOfProduct}<br/>
-            sugars: {sugarsOfProduct}<br/>
-            photo: {photoOfProduct}
-        </p>
-    </div>
-)
 export default FoodDetails
