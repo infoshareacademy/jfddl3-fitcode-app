6a02b8dfbdcebb76d739f94c15f1cf1b7303a7dd
Wojtek Pyda <w.pyda@poczta.onet.pl>
2018-01-16 17:18:40 +0100

    json database

--- src/FoodAdd.js
+++ src/FoodAdd.js
@@ -1,9 +1,61 @@
-import React from 'react';
+import React, {Component} from 'react';
 
-const FoodAdd = () => (
-    <div>
-        <h2>Add New Foooooooood</h2>
-    </div>
-)
+import TextField from 'material-ui/TextField';
+import RaisedButton from 'material-ui/RaisedButton';
 
-export default FoodAdd
+import textCategories from './text-categories'
+
+class FoodAdd extends Component {
+
+    state = {
+        data: null,
+        value: ''
+    }
+
+    componentWillMount() {
+        fetch(
+            `${process.env.PUBLIC_URL}/database.json`
+        )
+            .then(response => response.json())
+            .then(parsedJSONData => {
+                this.setState({
+                    data: parsedJSONData
+                })
+            })
+    }
+
+    handleSubmit = (event) => {
+        event.preventDefault()
+
+        // const obj = {'name': }
+        //
+        // console.log(obj)
+        // onChange na textField -> state, value w TextField-> state
+
+    }
+
+    render() {
+        return (
+            <div>
+                <form onSubmit={this.handleSubmit}>
+                {
+                    textCategories.map(cat => (
+                        <TextField
+                            hintText={cat.hintText}
+                            floatingLabelText={cat.floatingLabelText}
+                            style={{display: 'block'}}
+                            key={cat.floatingLabelText}
+                            name={cat.name}
+                            value={}
+                        />
+                    ))
+                }
+
+                <RaisedButton label="Dodaj" primary={true} type="submit" />
+                </form>
+            </div>
+        )
+    }
+}
+
+export default FoodAdd;
--- src/text-categories.js
+++ src/text-categories.js
@@ -3,37 +3,44 @@ import FoodAdd from './FoodAdd'
 const textCategories = [
     {
         hintText: 'Jedzenie',
-        floatingLabelText: 'Nazwa'
+        floatingLabelText: 'Nazwa',
+        name: 'food'
     },
 
     {
         hintText: 'Owoce, warzywa, itp',
-        floatingLabelText: 'Kategoria'
+        floatingLabelText: 'Kategoria',
+        name: 'grocery'
     },
 
     {
-        hintText: 'Kcal',
-        floatingLabelText: 'Kalorie'
+        hintText: 'kcal',
+        floatingLabelText: 'Kalorie',
+        name: 'kcal'
     },
 
     {
-        hintText: 'Białko',
-        floatingLabelText: 'g'
+        hintText: 'g',
+        floatingLabelText: 'Białko',
+        name: 'proteins'
     },
 
     {
-        hintText: 'Tłuszcze',
-        floatingLabelText: 'g'
+        hintText: 'g',
+        floatingLabelText: 'Tłuszcze',
+        name: 'fats'
     },
 
     {
-        hintText: 'Węglowodany',
-        floatingLabelText: 'g'
+        hintText: 'g',
+        floatingLabelText: 'Węglowodany',
+        name: 'carbo'
     },
 
     {
-        hintText: 'Cukry',
-        floatingLabelText: 'g'
+        hintText: 'g',
+        floatingLabelText: 'Cukry',
+        name: 'sug'
     },
 ]
 
