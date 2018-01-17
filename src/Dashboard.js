acf6a77bb475144b8b58f21ef9a0b27e6e616d96
Natalia Pilewska <pilewska.natalia@gmail.com>
2018-01-17 16:58:44 +0100

    Chart pie added

--- src/Dashboard.js
+++ src/Dashboard.js
@@ -1,9 +1,35 @@
 import React from 'react';
+import { render } from 'react-dom';
+import { PieChart, Pie, Tooltip }  from 'recharts'
+
+const data = [
+    {
+        value: 40,
+        name: 'warzywa'
+    },
+    {
+        value: 40,
+        name: 'owoce'
+    },
+    {
+        value: 20,
+        name: 'produkty mięsne'
+    }
+]
 
 const Dashboard = () => (
-    <div>
-        <h2>Dashboard</h2>
+    <div >
+        <PieChart width={500} height={500}>
+            <Pie
+                data={data}
+                dataKey="value"
+                nameKey="name"
+                fill="#8884d8"
+            />
+            <Tooltip />
+        </PieChart>
     </div>
-)
+);
+
 
 export default Dashboard
--- src/FoodList.js
+++ src/FoodList.js
@@ -1,12 +1,24 @@
 import React, {Component} from 'react';
+import {Link} from 'react-router-dom';
 import Avatar from 'material-ui/Avatar';
 import {List, ListItem} from 'material-ui/List';
 import Subheader from 'material-ui/Subheader';
-import ActionFavorite from 'material-ui/svg-icons/action/favorite';
+//mport favorite from 'material-ui/svg-icons/action/favorite';
+import ActionFavoriteBorder from 'material-ui/svg-icons/action/favorite-border';
 
-class FoodList extends React.Component {
+import TextField from 'material-ui/TextField';
+import Slider from 'material-ui/Slider';
+import SelectField from 'material-ui/SelectField';
+import MenuItem from 'material-ui/MenuItem';
+
+
+
+class FoodList extends Component {
     state = {
-        data: null
+        data: null,
+        foodName: '',
+        kcalSlider: 150,
+        catSelect: 'all',
     }
 
 
@@ -22,23 +34,81 @@ class FoodList extends React.Component {
             })
     }
 
+
+    handleFoodName = (event, value) => {
+        this.setState({foodName: value});
+    };
+
+    handleKcalSlider = (event, value) => {
+        this.setState({kcalSlider: value});
+    };
+
+    handleCatSelect = (event, index, value) => this.setState({catSelect: value})
+
     render() {
         return (
-            <div><List> <Subheader>Test Food List</Subheader>
-                {
-                    this.state.data && this.state.data
-                        .map(
-                            product => (
-                                <ListItem
-                                    primaryText={product.name}
-                                    secondaryText={product.energy}
-                                    leftAvatar={<Avatar src={product.photo}/>}
-                                    rightIcon={<ActionFavorite/>}
-                                />
-                            ))
-                }
-            </List></div>
+            <div>
+
+                <div>
+                    <TextField
+                        // hintText="Hint Text"
+                        floatingLabelText="Search your foodie..."
+                        onChange={this.handleFoodName}
+                    />
+                    <p>
+                        <span>{this.state.foodName} , you say</span>
+                    </p>
+                    <Slider
+                        min={0}
+                        max={300}
+                        step={30}
+                        value={this.state.kcalSlider}
+                        onChange={this.handleKcalSlider}
+                    />
+                    <p>
+                        <span>{this.state.kcalSlider} Kcal</span>
+                    </p>
+                    <SelectField
+                        floatingLabelText="Categories"
+                        value={this.state.catSelect}
+                        onChange={this.handleCatSelect}
+                    >
+                        <MenuItem value={'warzywa'} primaryText="warzywa" />
+                        <MenuItem value={'owoce'} primaryText="owoce" />
+                        <MenuItem value={3} primaryText="mieso" />
+                        <MenuItem value={4} primaryText="ryby" />
+                        <MenuItem value={5} primaryText="nabial" />
+                    </SelectField>
+                </div>
+
+
+                <List><Subheader>Test Food List</Subheader>
+                    {
+                        this.state.data && this.state.data
+                            .filter(product => product.name.indexOf(this.state.foodName) !== -1)
+                            .filter(product => this.state.catSelect === 'all' ? true : product.category === this.state.catSelect)
+                            .filter(product => product.energy < this.state.kcalSlider )
+                            .map(
+                                product => (
+                                    <Link
+                                        to={`/food-details/${product.uid}`}
+                                        style={{textDecoration: 'none'}}
+                                        key={product.uid}
+                                    >
+                                        <ListItem
+                                            primaryText={product.name}
+                                            secondaryText={`Kcal: ${product.energy} | ${product.category}`}
+                                            leftAvatar={<Avatar src={product.photo}/>}
+                                            rightIcon={<ActionFavoriteBorder/>}
+                                        />
+                                    </Link>
+                                ))
+                    }
+                </List>
+
+            </div>
         )
     }
 }
+
 export default FoodList
