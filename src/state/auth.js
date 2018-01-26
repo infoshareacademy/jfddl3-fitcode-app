c827bb8d6818d52486ff4a89e554c55ada4ee4a3
darkroom51 <darkroom@wp.pl>
2018-01-26 11:26:59 +0100

    Logs added;

--- src/state/auth.js
+++ src/state/auth.js
@@ -1,17 +1,42 @@
-import {auth,googleProvider} from '../firebase'
+import {database, auth,googleProvider} from '../firebase'
 
 const SET_USER = 'auth/SET_USER'
+const SET_LOGIN_LOGS = 'auth/SET_LOGIN_LOGS'
 
-const setUser = (user) => ({
+const setUser = (user) => ({ //kreator akcji zwyklej
     type: SET_USER,
     userData: user
+})
+
+const setLoginLogs = (logs) => ({
+    type: SET_LOGIN_LOGS,
+    logsData: logs
 })
 
 export const initAuth = () => (dispatch, getState) => {
     auth.onAuthStateChanged((user) => {
         // if not logged in user is null !
         dispatch(setUser(user))
+
+        if(user){ //if not null user is logged in, so set his record in DB
+            dispatch(logLoginDate())
+            dispatch(syncLoginLogs())
+        }
     })
+}
+
+const syncLoginLogs = () => (dispatch, getState) =>{
+    const uid = getState().auth.user.uid   //pobierz ze storu reduxa stan przez getState()
+    database.ref(`/users/${uid}/loginLogs`)
+        .on('value', (snapshot)=>dispatch(setLoginLogs(snapshot.val())))
+}
+
+const logLoginDate = () => (dispatch, getState) => {
+    const uid = getState().auth.user.uid   //pobierz ze storu reduxa stan przez getState()
+    database.ref(`/users/${uid}/loginLogs`)
+        .push({timestamp: Date.now()})
+        .then(() => console.log('LogIn Date pushed to DB!'))
+        .catch(() => console.log('LogIn Date failed!'))
 }
 
 export const logIn = (email, password) => (dispatch, getState) => {
@@ -39,9 +64,15 @@ export const logInWithGoogle = () => (dispatch, getState) => {
 }
 
 
+
+
+
 const initialState = {
     user: null
 }
+
+
+
 
 export default (state = initialState, action) => {
     switch (action.type) {
@@ -50,6 +81,12 @@ export default (state = initialState, action) => {
                 ...state,
                 user: action.userData
             }
+        case SET_LOGIN_LOGS:{
+            return {
+                ...state,
+                loginLogs: action.logsData
+            }
+        }
         default:
             return state
     }
